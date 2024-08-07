d39a0ba38d06e5a204434118565feb7772122a0b
BachiBachBach <thehessboi@gmail.com>
2024-08-07 16:47:33 -0500

    about to add in some code after testing it in a online compiler to test it if it works.

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@ double modulusQuiz();
 double pemmdasQuiz();
 void printScores(vector<double> modulus, vector<double> pemmdas);
 int generateNumber(int start, int end);
+vector<string> tokenize(string &expression);
+int evalulateStringAsMathExpression(vector<string> &tokens);
 
 int main()
 {
@@ -16,6 +18,16 @@ int main()
     vector<double> modulusScores;
     vector<double> pemmdasScores;
 
+	string part = "2+3+4+5+6+7+8+9";
+	vector<string> tokens = tokenize(part);
+
+	for (string part : tokens)
+	{
+		cout << part << endl;
+	}
+
+
+	return 0;
     do
     {
         cout << "Welcome to PEMMDAS practice.  " << endl
@@ -40,6 +52,7 @@ int main()
             break;
         case '3':
             printScores(modulusScores, pemmdasScores);
+			cout << endl;
             break;
         case '4':
             return 0;
@@ -96,20 +109,21 @@ double pemmdasQuiz()
     cout << "Enter how many problems you want to solve: ";
     cin >> numberOfProblems;
 
-    for (int i = 0; i < numberOfProblems; i++)
+	vector<char> characters;
+	string expression;
+	for (int i = 0; i < numberOfProblems; i++)
     {
-        cout
-            << "<------------------------------------------------------>" << endl
-            << generateNumber(1, 20) << ' ' << operators[generateNumber(0, 4)] << ' '
-            << generateNumber(1, 20) << ' ' << operators[generateNumber(0, 4)] << ' '
-            << generateNumber(1, 20) << ' ' << operators[generateNumber(0, 4)] << ' '
-            << generateNumber(1, 20) << ' ' << operators[generateNumber(0, 4)] << ' '
-            << generateNumber(1, 20) << ' ' << operators[generateNumber(0, 4)] << ' '
-            << generateNumber(1, 20) << ' ' << operators[generateNumber(0, 4)] << ' '
-            << generateNumber(1, 20) << ' ' << operators[generateNumber(0, 4)] << ' '
-            << generateNumber(1, 20) << ' ' << operators[generateNumber(0, 4)] << ' '
-            << generateNumber(1, 20) << endl
-            << "<------------------------------------------------------>" << endl;
+		expression = std::to_string(generateNumber(1, 9)) + operators[generateNumber(0, 4)] +
+	                         std::to_string(generateNumber(1, 9)) + operators[generateNumber(0, 4)] +
+	                         std::to_string(generateNumber(1, 9)) +   operators[generateNumber(0, 4)] +
+	                         std::to_string(generateNumber(1, 9)) + operators[generateNumber(0, 4)] +
+	                         std::to_string(generateNumber(1, 9)) +  operators[generateNumber(0, 4)] +
+	                         std::to_string(generateNumber(1, 9)) +   operators[generateNumber(0, 4)] +
+	                         std::to_string(generateNumber(1, 9)) +   operators[generateNumber(0, 4)] +
+	                         std::to_string(generateNumber(1, 9)) +    operators[generateNumber(0, 4)] +
+	                         std::to_string(generateNumber(1, 9));
+
+        cout << expression << endl;
         cout << "Answer here: ";
         cin >> answer;
 
@@ -125,7 +139,7 @@ void printScores(vector<double> modulus, vector<double> pemmdas)
     cout << "Your percentages on modulus quizzes are:" << endl;
     for (double percent : modulus)
     {
-        cout << modCount << ":\t" << percent << endl;
+        cout << modCount << ":\t" << percent << "%" << endl;
         modCount++;
     }
     cout << endl
@@ -133,7 +147,7 @@ void printScores(vector<double> modulus, vector<double> pemmdas)
     cout << "Your percentages on PEMMDAS quizzes are:" << endl;
     for (double percent : pemmdas)
     {
-        cout << modCount << ":\t" << percent << endl;
+        cout << modCount << ":\t" << percent << "%" << endl;
         modCount++;
     }
 }
@@ -143,3 +157,51 @@ int generateNumber(int start, int end)
     int randomNumber = (rand() % end) + start;
     return randomNumber;
 }
+
+
+
+vector<string> tokenize(string &expression)
+{
+
+	vector<string> tokens;
+	size_t parts = expression.size() / 3;
+
+	int pos = 0;
+	while ( pos < expression.size() - 2)
+	{
+		string token = "";
+
+		token += expression[pos];
+		pos++;
+		token += expression[pos];
+		pos++;
+		token += expression[pos];
+
+
+
+		tokens.push_back(token);
+
+
+	}
+	unsigned int counter = 0;
+
+
+	for (string &token : tokens)
+	{
+		if (counter % 2 != 0 && (counter + 1) <= tokens.size())
+		{
+			tokens.erase(tokens.begin()+counter);
+		}
+		counter++;
+	}
+
+	return tokens;
+
+}
+
+int evalulateStringAsMathExpression()
+{
+
+}
+
+
